f8002b375bdb1afad9946ca3dde96c64d23fadba
Leonardo Alcântara <83440954+Leolsalvador@users.noreply.github.com>
2024-09-19 00:05:59 -0300

    fix: database

--- api/settings.py
+++ api/settings.py
@@ -86,22 +86,22 @@ WSGI_APPLICATION = 'api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config("NAME_DATABASE"),
-        'USER': config("USER_DATABASE"),
-        'PASSWORD': config("PASSWORD_DATABASE"),
-        'HOST': config("HOST_DATABASE"),
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': config("NAME_DATABASE"),
+#         'USER': config("USER_DATABASE"),
+#         'PASSWORD': config("PASSWORD_DATABASE"),
+#         'HOST': config("HOST_DATABASE"),
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
